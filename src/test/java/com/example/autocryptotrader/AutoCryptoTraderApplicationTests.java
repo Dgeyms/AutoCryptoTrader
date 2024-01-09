28bb505b7cebb3f04040f64f51164aa810dfefeb
Dgeyms <Dgeyms@gmail.com>
2024-01-09 23:52:41 +0300

    feat(products) create class AutoCryptoTraderApplicationTests

---
+++ src/test/java/com/example/autocryptotrader/AutoCryptoTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autocryptotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AutoCryptoTraderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
