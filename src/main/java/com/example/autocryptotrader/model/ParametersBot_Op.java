1bbc8cd90e6583eb1b2aa4636cc018d29583a7ad
dgeyms <dgeyms@dgeyms.com>
2024-05-15 10:56:19 +0300

    feat(product): add class ParametersBot_Op.java

---
+++ src/main/java/com/example/autocryptotrader/model/ParametersBot_Op.java
@@ -0,0 +1,10 @@
+package com.example.autocryptotrader.model;
+
+public class ParametersBot_Op {
+    private Long botId;
+    private final String tokenPair = "OP/USDT";
+    private final String nameBot = "OP long 15";
+    private final String directionOfTrade = "long"; // направление торговли
+    private final int timePeriod = 15;
+    private final double dollarOrderVolume = 10.0;
+}
